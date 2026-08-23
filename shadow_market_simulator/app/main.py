06098b65c3e6f7299f39fb41d367dac97c7005e2
Kanneytari <hitroplan@yandex.ru>
2026-08-23 22:53:59 +0300

    Add retail allocation controls

--- shadow_market_simulator/app/main.py
+++ shadow_market_simulator/app/main.py
@@ -21,6 +21,7 @@ from .recruitment_runtime import NightshiftRecruitmentService
 from .simulation import iso, utcnow
 from .storefront_handlers import build_storefront_router
 from .time_handlers import build_time_router
+from .workflow_allocation_handlers import build_workflow_allocation_router
 from .workflow_dashboard_handlers import build_workflow_dashboard_router
 from .workflow_final import FinalWorkflowGameService, FinalWorkflowSimulationEngine
 from .workflow_handlers import build_workflow_router
@@ -80,6 +81,7 @@ async def main() -> None:
     # Specific flows go first; compatibility routers remain as fallbacks.
     dispatcher.include_router(build_workflow_dashboard_router(db, game, simulation))
     dispatcher.include_router(build_workflow_reassign_router(game))
+    dispatcher.include_router(build_workflow_allocation_router(game))
     dispatcher.include_router(build_workflow_router(game))
     dispatcher.include_router(build_operations_router(game))
     dispatcher.include_router(build_dispute_router(game))
