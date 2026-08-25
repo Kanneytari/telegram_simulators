32fae90d6290511c268fff3c36f586a616024b8a
Kanneytari <hitroplan@yandex.ru>
2026-08-25 19:18:26 +0300

    refactor(shadow-market): consume courier feature package API

--- shadow_market_simulator/app/bootstrap.py
+++ shadow_market_simulator/app/bootstrap.py
@@ -14,7 +14,7 @@ from .core import Database, Settings, load_settings
 from .courier_management import CourierManagementGameService, CourierManagementSimulationEngine
 from .gameplay_updates import apply_gameplay_updates
 from .inbox import install_inbox_lifecycle
-from .staff.couriers.recruitment import CourierRecruitmentService
+from .staff.couriers import CourierRecruitmentService
 from .tutorial import apply_tutorial_updates, build_tutorial_router
 from .tutorial_copy_update import apply_tutorial_copy_update
 from .tutorial_runtime import apply_tutorial_runtime_fixes
