52c7e62120e288f07244f921cf8bc60d6a6aa515
Kanneytari <hitroplan@yandex.ru>
2026-08-25 19:44:31 +0300

    refactor(shadow-market): bridge courier model dependency

---
+++ shadow_market_simulator/app/staff/couriers/courier_model.py
@@ -0,0 +1,25 @@
+from .model import (
+    TRAIT_CONCEALS,
+    TRAIT_LEARNER,
+    TRAIT_METICULOUS,
+    TRAIT_OVERHEATS,
+    TRAIT_PRESSURE_PROOF,
+    TRAIT_SENSITIVE,
+    TRAIT_STEADY,
+    condition_band,
+    pace_band,
+    relationship_band,
+)
+
+__all__ = [
+    "TRAIT_CONCEALS",
+    "TRAIT_LEARNER",
+    "TRAIT_METICULOUS",
+    "TRAIT_OVERHEATS",
+    "TRAIT_PRESSURE_PROOF",
+    "TRAIT_SENSITIVE",
+    "TRAIT_STEADY",
+    "condition_band",
+    "pace_band",
+    "relationship_band",
+]
